5746e094bd3719fe3b8f3d1a09123d3479ca5a4b
Chee Leong <klrkdekira@gmail.com>
2017-12-11 11:58:14 +0800

    chore: add documentation to FileStore, DiskStorage, S3Storage and MinioStorage

--- filestore.go
+++ filestore.go
@@ -1,17 +1,25 @@
 package lookie
 
+// FileStore
+// TODO stub for https://github.com/engineersmy/lookie/issues/9
 type FileStore interface {
 
 }
 
+// DiskStorage
+// TODO stub for https://github.com/engineersmy/lookie/issues/10
 type DiskStorage struct {
 
 }
 
+// S3Storage
+// TODO stub for https://github.com/engineersmy/lookie/issues/11
 type S3Storage struct {
 
 }
 
+// MinioStorage
+// TODO stub for https://github.com/engineersmy/lookie/issues/12
 type MinioStorage struct {
 
 }
